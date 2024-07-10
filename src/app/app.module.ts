e938d8dfc30aeae959b21757cf86422fe4f86c8e
wevkleyton <kleyton.cpd@gmail.com>
2024-07-10 14:10:48 -0300

    criacao de projeto componensts para estudo angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
   ],
   imports: [
     BrowserModule,
