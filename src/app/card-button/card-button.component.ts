ea57966475c7452e9339c71bb9618dd15b5b9c50
wevkleyton <kleyton.cpd@gmail.com>
2024-07-12 14:55:23 -0300

    funcionamento de template dentro do componente

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
-  templateUrl: './card-button.component.html',
+  // templateUrl: './card-button.component.html',
+  template: `<div class="card-button">Teste</div>`,
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
