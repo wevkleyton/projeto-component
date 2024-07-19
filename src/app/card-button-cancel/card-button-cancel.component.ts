a2d5c6e58074c30aeccf2c3dec63d62551e6e37f
wev-kleyton <kleyton.wev@gmail.com>
2024-07-19 12:22:28 -0300

    importar Arquivos deEstilo com @import

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,16 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styleUrls: ['./card-button-cancel.component.scss'],
-  styles:[
-    `
-    div.card-cancel-button{
-      background-color: green;
-    }
-    `
-  ],
-  
-  // ,'./card-button-cancel-2.component.scss'
+  styleUrls: ['./card-button-cancel.component.scss'],    
 })
 export class CardButtonCancelComponent {
 
