107b39d1f73fb217cae707091ccd6a479dc73b7f
wev-kleyton <kleyton.wev@gmail.com>
2024-07-19 11:59:03 -0300

    Estilos dentro do template

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styleUrls: ['./card-button-cancel.component.scss','./card-button-cancel-2.component.scss']
+  styleUrls: ['./card-button-cancel.component.scss'],
+  styles:[
+    `
+    div.card-cancel-button{
+      background-color: green;
+    }
+    `
+  ],
   
   // ,'./card-button-cancel-2.component.scss'
 })
