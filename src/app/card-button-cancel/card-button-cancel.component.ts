e77056d2dc7e2097316d7c5f0cc8c15ecdf72f16
wevkleyton <kleyton.cpd@gmail.com>
2024-07-19 10:23:39 -0300

    Referencia Arquivos de Estilo prop styleurls

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,21 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  // styleUrl: './card-button-cancel.component.scss'
-  styles: [
-    `
-  .card-cancel-button {
-    font-size: 16px;
-    text-align: center;
-    margin-top: 10px;
-    border-radius: 25px;
-    background-color: red;
-    cursor: pointer;
-    font-weight: bold;
-    padding: 10px;
-  }
-    `
-  ],
+  styleUrls: ['./card-button-cancel.component.scss','./card-button-cancel-2.component.scss']
+  
+  // ,'./card-button-cancel-2.component.scss'
 })
 export class CardButtonCancelComponent {
 
