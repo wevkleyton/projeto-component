2bd11ecddd9d7fe7573d4e7b1c6e3258f819e121
wevkleyton <kleyton.cpd@gmail.com>
2024-08-01 12:27:55 -0300

    View Encapsulation None

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CardComponent {
   plano = {
